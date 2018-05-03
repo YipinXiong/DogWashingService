d073645834478be9029ca8ce3836b1bb3e44b341
Yipin <xiong1pin@gmail.com>
2018-05-03 19:26:17 +1000

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var MongoClient = require('mongodb').MongoClient, assert =require('assert');
+
+var url = 'mongodb://localhost:27017/myproject';
+
+MongoClient.connect(url, (err,db)=>{
+	assert.equal(null,err);
+	console.log('Connected successfully to server');
+	db.close();
+});
